d0c8376f62ad59816a23471038e4f7444276d47c
matematikbahcem <matematikbahcemm@gmail.com>
2025-01-01 02:53:09 +0100

    fiz pagi

--- .vercel/output/static/_astro/index.astro_astro_type_script_index_0_lang.BhPgaj46.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./swiper.Q9gGecdY.js";document.addEventListener("DOMContentLoaded",()=>{new e(".swiper",{autoplay:!0,slidesPerView:1,breakpoints:{640:{slidesPerView:2,spaceBetween:20},768:{slidesPerView:3,spaceBetween:30},1024:{slidesPerView:4,spaceBetween:30}},spaceBetween:30,loop:!0,scrollbar:{el:".swiper-scrollbar"}})});
--- .vercel/output/static/_astro/studio-component.BvzCXHfs.js
+++
@@ -1,1 +0,0 @@
-import{as as p}from"./studio-component.DtClU_0u.js";import"./client.Dc_o9dEB.js";export{p as StudioComponent};
